a181859b147fcd0e0d26adcfedd08e53050c2345
swellwrench <swellwrench@gmail.com>
2020-06-11 20:25:50 -0500

    Studio complete

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,12 +1,30 @@
 package org.launchcode.spaday.models;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+    @Size(min=5, max=15)
     private String username;
+    @Email
     private String email;
+    @Size(min=6)
     private String password;
+    @Size(min=6)
+    @NotNull(message="Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
+    }
+
+    private void checkPassword(@RequestParam String password, @RequestParam String verifyPassword) {
+        if(password != null && verifyPassword != null && password != verifyPassword) {
+            verifyPassword = null;
+        }
     }
 
     public User(String username, String email, String password) {
@@ -37,6 +55,16 @@ public class User {
     }
 
     public void setPassword(String password) {
+        checkPassword(password, verifyPassword);
         this.password = password;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        checkPassword(password, verifyPassword);
+        this.verifyPassword = verifyPassword;
     }
 }
